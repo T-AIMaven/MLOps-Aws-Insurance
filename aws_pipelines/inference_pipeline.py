7f5b1203767203b5932522aaa3e7f7b8252e8cfc
satel33 <jurijs.haricevs@gmail.com>
2025-01-25 12:24:06 -0300

    feat: implement inference_pipeline

--- aws_pipelines/inference_pipeline.py
+++ aws_pipelines/inference_pipeline.py
@@ -17,6 +17,7 @@ from aws_pipelines.cond_register_pipeline import (
     create_fail_step,
     create_registration_step,
 )
+
 from aws_pipelines.deployment_pipeline import create_condition_step
 from aws_pipelines.evaluate_pipeline import create_evaluation_step
 from aws_pipelines.preprocessing_pipeline import (
